22073816e24489219cb5e1bc325140bdd07cbd5b
Nelson Chan <khchanel@gmail.com>
2013-06-04 16:37:20 +1000

    change follow text on click function

--- prototype/js/main.js
+++ prototype/js/main.js
@@ -5,3 +5,11 @@ function back() {
 function forward() {
 	window.history.forward();
 }
+
+function change_follow() {
+	if ($("#follow-txt").html() === "Followed") {
+		$("#follow-txt").html("Follow");
+	} else {
+		$("#follow-txt").html("Followed");
+	}
+}
